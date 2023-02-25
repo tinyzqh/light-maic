b65847e6be00b4c77842328630487c48b272bb8d
hzq <tinyzqh@163.com>
2023-02-25 11:59:20 +0800

    [dev] delete useless info

--- qplex_smac/smac/env/starcraft2/fake_starcraft2.py
+++ qplex_smac/smac/env/starcraft2/fake_starcraft2.py
@@ -34,238 +34,33 @@ class FakeStarCraft2Env(MultiAgentEnv):
     """
     def __init__(
         self,
-        map_name="8m",
-        step_mul=8,
-        move_amount=2,
-        difficulty="7",
-        game_version=None,
-        seed=None,
-        continuing_episode=False,
-        obs_all_health=True,
-        obs_own_health=True,
-        obs_last_action=False,
-        obs_pathing_grid=False,
-        obs_terrain_height=False,
-        obs_instead_of_state=False,
-        obs_timestep_number=False,
-        state_last_action=True,
-        state_timestep_number=False,
-        reward_sparse=False,
-        reward_only_positive=True,
-        reward_death_value=10,
-        reward_win=200,
-        reward_defeat=0,
-        reward_negative_scale=0.5,
-        reward_scale=True,
-        reward_scale_rate=20,
-        replay_dir="",
-        replay_prefix="",
-        window_size_x=1920,
-        window_size_y=1200,
-        heuristic_ai=False,
-        heuristic_rest=False,
-        debug=False,
+        map_name="fake",
+        seed=None
     ):
-        """
-        Create a StarCraftC2Env environment.
 
-        Parameters
-        ----------
-        map_name : str, optional
-            The name of the SC2 map to play (default is "8m"). The full list
-            can be found by running bin/map_list.
-        step_mul : int, optional
-            How many game steps per agent step (default is 8). None
-            indicates to use the default map step_mul.
-        move_amount : float, optional
-            How far away units are ordered to move per step (default is 2).
-        difficulty : str, optional
-            The difficulty of built-in computer AI bot (default is "7").
-        game_version : str, optional
-            StarCraft II game version (default is None). None indicates the
-            latest version.
-        seed : int, optional
-            Random seed used during game initialisation. This allows to
-        continuing_episode : bool, optional
-            Whether to consider episodes continuing or finished after time
-            limit is reached (default is False).
-        obs_all_health : bool, optional
-            Agents receive the health of all units (in the sight range) as part
-            of observations (default is True).
-        obs_own_health : bool, optional
-            Agents receive their own health as a part of observations (default
-            is False). This flag is ignored when obs_all_health == True.
-        obs_last_action : bool, optional
-            Agents receive the last actions of all units (in the sight range)
-            as part of observations (default is False).
-        obs_pathing_grid : bool, optional
-            Whether observations include pathing values surrounding the agent
-            (default is False).
-        obs_terrain_height : bool, optional
-            Whether observations include terrain height values surrounding the
-            agent (default is False).
-        obs_instead_of_state : bool, optional
-            Use combination of all agents' observations as the global state
-            (default is False).
-        obs_timestep_number : bool, optional
-            Whether observations include the current timestep of the episode
-            (default is False).
-        state_last_action : bool, optional
-            Include the last actions of all agents as part of the global state
-            (default is True).
-        state_timestep_number : bool, optional
-            Whether the state include the current timestep of the episode
-            (default is False).
-        reward_sparse : bool, optional
-            Receive 1/-1 reward for winning/loosing an episode (default is
-            False). Whe rest of reward parameters are ignored if True.
-        reward_only_positive : bool, optional
-            Reward is always positive (default is True).
-        reward_death_value : float, optional
-            The amount of reward received for killing an enemy unit (default
-            is 10). This is also the negative penalty for having an allied unit
-            killed if reward_only_positive == False.
-        reward_win : float, optional
-            The reward for winning in an episode (default is 200).
-        reward_defeat : float, optional
-            The reward for loosing in an episode (default is 0). This value
-            should be nonpositive.
-        reward_negative_scale : float, optional
-            Scaling factor for negative rewards (default is 0.5). This
-            parameter is ignored when reward_only_positive == True.
-        reward_scale : bool, optional
-            Whether or not to scale the reward (default is True).
-        reward_scale_rate : float, optional
-            Reward scale rate (default is 20). When reward_scale == True, the
-            reward received by the agents is divided by (max_reward /
-            reward_scale_rate), where max_reward is the maximum possible
-            reward per episode without considering the shield regeneration
-            of Protoss units.
-        replay_dir : str, optional
-            The directory to save replays (default is None). If None, the
-            replay will be saved in Replays directory where StarCraft II is
-            installed.
-        replay_prefix : str, optional
-            The prefix of the replay to be saved (default is None). If None,
-            the name of the map will be used.
-        window_size_x : int, optional
-            The length of StarCraft II window size (default is 1920).
-        window_size_y: int, optional
-            The height of StarCraft II window size (default is 1200).
-        heuristic_ai: bool, optional
-            Whether or not to use a non-learning heuristic AI (default False).
-        heuristic_rest: bool, optional
-            At any moment, restrict the actions of the heuristic AI to be
-            chosen from actions available to RL agents (default is False).
-            Ignored if heuristic_ai == False.
-        debug: bool, optional
-            Log messages about observations, state, actions and rewards for
-            debugging purposes (default is False).
-        """
-        # Map arguments
-        self.map_name = map_name
-        # map_params = get_map_params(self.map_name)
-        self.n_agents = 10  # map_params["n_agents"]  智能体的数量
-        self.n_enemies = 12  # map_params["n_enemies"]  敌方数量
-        self.episode_limit = 150  # map_params["limit"]
-        self._move_amount = move_amount
-        self._step_mul = step_mul
-        self.difficulty = difficulty
-
-        # Observations and state
-        self.obs_own_health = obs_own_health
-        self.obs_all_health = obs_all_health
-        self.obs_instead_of_state = obs_instead_of_state
-        self.obs_last_action = obs_last_action
-        self.obs_pathing_grid = obs_pathing_grid
-        self.obs_terrain_height = obs_terrain_height
-        self.obs_timestep_number = obs_timestep_number
-        self.state_last_action = state_last_action
-        self.state_timestep_number = state_timestep_number
-        if self.obs_all_health:
-            self.obs_own_health = True
-        self.n_obs_pathing = 8
-        self.n_obs_height = 9
-
-        # Rewards args
-        self.reward_sparse = reward_sparse
-        self.reward_only_positive = reward_only_positive
-        self.reward_negative_scale = reward_negative_scale
-        self.reward_death_value = reward_death_value
-        self.reward_win = reward_win
-        self.reward_defeat = reward_defeat
-        self.reward_scale = reward_scale
-        self.reward_scale_rate = reward_scale_rate
+        self.n_agents = 10  # 智能体的数量
+        self.episode_limit = 150
 
         # Other
-        self.game_version = game_version
-        self.continuing_episode = continuing_episode
         self._seed = seed
-        self.heuristic_ai = heuristic_ai
-        self.heuristic_rest = heuristic_rest
-        self.debug = debug
-        self.window_size = (window_size_x, window_size_y)
-        self.replay_dir = replay_dir
-        self.replay_prefix = replay_prefix
 
         # Actions
         self.n_actions_no_attack = 6
         self.n_actions_move = 4
-        self.n_actions = self.n_actions_no_attack + self.n_enemies
+        self.n_actions = 18
 
-        # Map info
-        self._agent_race = 'T'  # map_params["a_race"]
-        self._bot_race = 'T'  # map_params["b_race"]
-        self.shield_bits_ally = 1 if self._agent_race == "P" else 0
-        self.shield_bits_enemy = 1 if self._bot_race == "P" else 0
-        self.unit_type_bits = 3  # map_params["unit_type_bits"]
-        self.map_type = 'MMM'  # map_params["map_type"]
-
-        self.max_reward = (
-            self.n_enemies * self.reward_death_value + self.reward_win
-        )
-
-        self.agents = {}
-        self.enemies = {}
-        self._episode_count = 0
-        self._episode_steps = 0
-        self._total_steps = 0
-        self._obs = None
         self.battles_won = 0
         self.battles_game = 0
         self.timeouts = 0
         self.force_restarts = 0
-        self.last_stats = None
-        self.death_tracker_ally = np.zeros(self.n_agents)
-        self.death_tracker_enemy = np.zeros(self.n_enemies)
-        self.previous_ally_units = None
-        self.previous_enemy_units = None
-        self.last_action = np.zeros((self.n_agents, self.n_actions))
-        self._min_unit_type = 0
-        self.marine_id = self.marauder_id = self.medivac_id = 0
-        self.hydralisk_id = self.zergling_id = self.baneling_id = 0
-        self.stalker_id = self.colossus_id = self.zealot_id = 0
-        self.max_distance_x = 0
-        self.max_distance_y = 0
-        self.map_x = 0
-        self.map_y = 0
-        self.terrain_height = None
-        self.pathing_grid = None
-        self._run_config = None
-        self._sc2_proc = None
-        self._controller = None
 
         # Qatten
-        self.unit_dim = 4 + self.shield_bits_ally + self.unit_type_bits
-
-        # Try to avoid leaking SC2 processes on shutdown
-        atexit.register(lambda: self.close())
+        self.unit_dim = 7
 
     def reset(self):
         """Reset the environment. Required after each full episode.
         Returns initial observations and states.
         """
-        self._episode_steps = 0
 
         return self.get_obs(), self.get_state()
 
@@ -275,7 +70,6 @@ class FakeStarCraft2Env(MultiAgentEnv):
         terminated = False if np.random.randn() < 0.9 else True
         info = {}
         return reward, terminated, info
-
 
     def get_total_actions(self):
         """Returns the total number of actions an agent could ever take."""
@@ -320,7 +114,7 @@ class FakeStarCraft2Env(MultiAgentEnv):
         """Returns the available actions of all agents in a list."""
         avail_actions = []
         for agent_id in range(self.n_agents):
-            avail_agent = [np.random.randint(0, 2) for _ in range(18)]
+            avail_agent = [np.random.randint(0, 2) for _ in range(self.n_actions)]
             avail_actions.append(avail_agent)
         return avail_actions
 
